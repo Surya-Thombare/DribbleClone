0f7ed97f9f59c36cf5937a98135f154524dcc976
suryakant thombare <suryakant.thombare@ispace.com>
2023-07-02 02:04:56 +0530

    Footer p2

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import { footerLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link';
-import React from 'react'
 
 type columnProps = {
   title: string;
@@ -36,7 +35,23 @@ function Footer() {
         </div>
         <div className='flex flex-wrap gap-12'>
           <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
+          <div className='flex-1 flex flex-col gap-4'>
+          <FooterColumn title={footerLinks[1].title} links={footerLinks[1].links}/>
+          <FooterColumn title={footerLinks[2].title} links={footerLinks[2].links}/>
+          </div>
+          <FooterColumn title={footerLinks[3].title} links={footerLinks[3].links}/>
+          <div className='flex-1 flex flex-col gap-4'>
+          <FooterColumn title={footerLinks[4].title} links={footerLinks[4].links}/>
+          <FooterColumn title={footerLinks[5].title} links={footerLinks[5].links}/>
+          </div>
+          <FooterColumn title={footerLinks[6].title} links={footerLinks[6].links}/>
         </div>
+      </div>
+      <div className='flexBetween footer_copyright'>
+        <p>@ 2023 dribble all rights reserved</p>
+        <p className='text-gray'>
+          <span className='text-black'>10,200</span> Projects submitted
+        </p>
       </div>
     </footer>
   )
